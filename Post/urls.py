df793ce57fec446d083dde72e92a971acf689d98
alexmwaura <alexmwaura43@gmail.com>
2019-11-18 10:59:13 +0300

    Add routes for the CRUD operations

--- Post/urls.py
+++ Post/urls.py
@@ -3,10 +3,21 @@ from . import views
 
 
 
+
 urlpatterns = [
 
     
     path('', views.index,name = 'insta-home'),
+    path('post/update/<int:pk>',views.PostUpdateView.as_view(), name ='post-update'),
+
+    path('post/<int:pk>/',views.PostDetailView.as_view(), name ='post-detail'),
+    path('post/<int:pk>/update',views.PostUpdateView.as_view(), name ='post-update'),
+
+    path('post/comment/<int:pk>/',views.CreateComment.as_view(), name ='post-comment'),
+
+
+    path('post/new/', views.CreatePost.as_view(), name='create-post'),
+
 
 
 ]
