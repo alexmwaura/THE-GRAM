2ae24bbcae9ede0a7b8cc04c44d11c984c13ef01
alexmwaura <alexmwaura43@gmail.com>
2019-11-18 10:55:23 +0300

    add models for the Post app with new tables created for comments and user followers

--- Post/models.py
+++ Post/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-
+from users.models import Profile
+from django.urls import reverse
+import datetime
 
 
 # Create your models here.
@@ -10,11 +12,70 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    title = models.TextField()
-    cover = models.ImageField(upload_to = 'images/')
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
-    date_posted = models.DateTimeField(default = timezone.now) 
+    profile_user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts',default = timezone.now)
+
+    title = models.CharField(max_length = 30)
+    cover = models.ImageField(upload_to = 'media/images')
+    account_user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='user')
+    date_posted = models.DateField(auto_now=True) 
+    rating_count = models.IntegerField(default=0)
+
+
+
+    class Meta:
+        ordering = ['-date_posted']
+    
+
+    def __str__(self):
+        return self.title
+
+
+
+
+
+
+    def get_absolute_url(self):
+        return reverse('insta-home',kwargs={'date_posted':self.date_posted})
+
+
+
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments',)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='posts',)  
+    text = models.TextField(max_length=250)
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+
+
+    class Meta:
+        ordering = ['created_date']  
+
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+
 
 
     def __str__(self):
-        return  self.title      
+        return 'Comment by {} on '.format(self.user, self.created_date)
+
+
+    def get_absolute_url(self):
+        return reverse('insta-home',)
+
+class FollowersAndFollowing(models.Model):
+    """
+    List of followers and following
+    """
+    
+    followed_by = models.ForeignKey(User, on_delete=models.CASCADE,  related_name='followed_by')
+    followed_to = models.ForeignKey(User, on_delete=models.CASCADE,  related_name='followed_to')
+    created_date = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.followed_by.first_name + '-' + str(self.followed_to.first_name)        
