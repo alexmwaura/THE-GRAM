74c9c89a6f7513f0beda7df5027d9c90aa0dd4b1
alexmwaura <alexmwaura43@gmail.com>
2019-11-19 15:04:17 +0300

    tails

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 STATIC_URL = '/static/'
